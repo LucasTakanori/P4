e840c5e7aaaf1002b4ef89139ccd7bd9bdb31022
ValVila <xavier.val@estudiantat.upc.edu>
2022-06-06 16:17:35 +0200

    graph well done

--- scripts/plotcoefs.py
+++ scripts/plotcoefs.py
@@ -1,13 +1,13 @@
 import matplotlib.pyplot as plt
 import numpy as np
-plt.rcParams['figure.dpi'] = 150
+plt.rcParams['figure.dpi'] = 100
 plt.rcParams.update({'font.size': 9})
 
 lp = np.loadtxt('lp_2_3.txt')
 lpcc = np.loadtxt('lpcc_2_3.txt')
 mfcc = np.loadtxt('mfcc_2_3.txt')
 fig, axs = plt.subplots(3, ncols=1)
-fig.suptitle("2nd vs 3rd coefficients", y=1.02)
+fig.suptitle("2nd vs 3rd coefficients", y=0.99)
 
 lp_2 = lp[:, 0]
 lp_3 = lp[:, 1]
@@ -27,6 +27,6 @@ axs[2].scatter(mfcc_2,mfcc_3, label="Mfcc",color='green', s=1)
 for i in range(3):
     axs[i].set_ylabel("3rd coefficient")
     axs[i].set_xlabel("2nd coefficient")
-    axs[i].legend(loc='upper right', prop={'size': 6})
+    axs[i].legend(loc='upper right', prop={'size': 5})
 
 plt.savefig("graph.png")
